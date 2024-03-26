40b98d290cea331b1326557f1133eda45ab3d167
Rdtns <146824397+Rdtns@users.noreply.github.com>
2024-03-26 10:55:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from flask import requests
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
